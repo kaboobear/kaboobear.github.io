350bce2cfac0dea200d13b88ef450d1cbd2c87e9
Kaboo <kaboo.bear@gmail.com>
2019-12-08 21:16:24 +0200

    first

---
+++ README.md
@@ -0,0 +1,71 @@
+<p align="center">
+    <img src="https://img.icons8.com/bubbles/100/000000/rocket.png" width="100" height="100">
+</p>
+
+<h1 align="center">Kaboo Bear</h1>
+
+[![Forks][forks-shield]][forks-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+
+
+<br>
+<br>
+
+ 
+
+<a href="https://kaboobear.github.io"> <h2>Developer Tips Project</h2> </a>
+
+### You can use it:
+* as a quick solution for your project
+* as your own framework
+* as building blocks for your page
+* just for fun
+
+<br>
+<br>
+
+
+<h2 align="center">Screen</h2>
+
+![Layout](kaboo.png)
+
+<br>
+<br>
+<br>
+
+<h2 align="center">Contact Me</h2>
+
+<div>
+    <p><b>Telegram:</b> @kaboo_bear </p>
+</div>
+
+<hr>
+
+<div>
+    <p><b>Gmail:</b> kaboo.bear@gmail.com </p>
+</div>
+
+<hr>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+[forks-shield]: https://img.shields.io/github/forks/kaboobear/kaboobear.github.io.svg?style=flat-square
+[forks-url]: https://github.com/kaboobear/kaboobear.github.io/network/members
+[issues-shield]: https://img.shields.io/github/issues/kaboobear/kaboobear.github.io.svg?style=flat-square
+[issues-url]: https://github.com/kaboobear/kaboobear.github.io/issues
+[license-shield]: https://img.shields.io/github/license/kaboobear/kaboobear.github.io.svg?style=flat-square
+[license-url]: https://github.com/kaboobear/kaboobear.github.io/blob/master/LICENSE.txt
+
+
---
+++ script.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+
+    // Match Height
+    // $('').matchHeight();
+
+
+
+    // Animation
+    // new WOW().init();
+
+
+
+    // $(".smooth").on('click', function () {
+    //     var elementClick = $(this).attr("href");
+    //     var destination = $(elementClick).offset().top;
+    //     jQuery("html:not(:animated),body:not(:animated)").animate({
+    //         scrollTop: destination
+    //     }, 800);
+    //     return false;
+    // });
+
+
+
+
+
+
+
+    $(".copy-btn").click(function () {
+        if (!$(this).hasClass('active')) {
+            var elem = $(this);
+            elem.prev().CopyToClipboard();
+            elem.addClass('rubberBand animated');
+
+            setTimeout(function () {
+                elem.removeClass('rubberBand animated');
+            }, 500);
+
+            $(this).addClass("active");
+
+            if ($(this).parent().parent().children('.copy-element').length === $(this).parent().parent().children('.copy-element').children('.active').length) {
+                $(this).parent().parent().addClass('active');
+                $(this).parent().parent().parent().children('.glob-title').addClass('active')
+            }
+        }
+
+    })
+
+
+
+
+
+    //Site Tabs
+
+    $('.site-tabs-buttons').on('click', 'li:not(.active)', function () {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .parent().next('.site-tabs-items').find('.site-tabs-item').fadeOut(0).removeClass('active').eq($(this).index()).fadeIn(500).addClass('active');
+    });
+
+
+
+    // To Top
+
+    $(window).on('scroll', function () {
+        if ($(document).scrollTop() > 300) {
+            $('.to-top').addClass('active');
+        } else {
+            $('.to-top').removeClass('active');
+        }
+    })
+
+    if ($(document).scrollTop() > 300) {
+        $('.to-top').addClass('active');
+    } else {
+        $('.to-top').removeClass('active');
+    }
+
+
+    $('.to-top').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, '300');
+    })
+
+
+
+
+
+    //Test
+
+
+})
+
+
+
+$(window).on('load', function () {
+    setTimeout(function () {
+        $('.preloader').addClass('active');
+        $('body').css('overflow', 'visible');
+    }, 1500)
+
+})
