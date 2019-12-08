c3677ac2f429a00fc184b34808382721810b8462
Kaboo Bear <38396902+kaboobear@users.noreply.github.com>
2019-12-08 21:09:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -89,46 +89,6 @@ $(document).ready(function () {
     //Test
 
 
-    const typedTextSpan = document.querySelector(".typed-text");
-    const cursorSpan = document.querySelector(".cursor");
-
-    const textArray = ["One", "Two", "Three"];
-    // const textArray = ["need technology to show up beauty of products"];
-    const typingDelay = 50;
-    const erasingDelay = 25;
-    const newTextDelay = 1000;
-    let textArrayIndex = 0;
-    let charIndex = 0;
-
-    function type() {
-        if (charIndex < textArray[textArrayIndex].length) {
-            if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-            charIndex++;
-            setTimeout(type, typingDelay);
-        } else {
-            cursorSpan.classList.remove("typing");
-            setTimeout(erase, newTextDelay);
-        }
-    }
-
-    function erase() {
-        if (charIndex > 0) {
-            if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
-            charIndex--;
-            setTimeout(erase, erasingDelay);
-        } else {
-            cursorSpan.classList.remove("typing");
-            textArrayIndex++;
-            if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-            setTimeout(type, typingDelay + 1100);
-        }
-    }
-
-    if (textArray.length) setTimeout(type, newTextDelay + 250);
-
-
 })
 
 
